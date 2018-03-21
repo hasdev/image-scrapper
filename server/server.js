1c19b967754a7d4227029c5c31a3931d798fd929
ghostMarshall <udhay.manhas@gmail.com>
2018-03-22 04:06:46 +0530

    update server

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static(publicPath));
+app.use('/',express.static(publicPath));
 
 app.post('/search', (req, res) => {
   var body = _.pick(req.body, ['keyword', 'timestamp']); //picking up required property only
